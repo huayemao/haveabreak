de888b1e340ba074ea9ca47681e3deb48821085e
huayemao <huayemao4g@gmail.com>
2026-05-11 17:20:56 +0800

    feat: 更新 card 页面 title

--- app/[lang]/card/layout.tsx
+++ app/[lang]/card/layout.tsx
@@ -10,7 +10,7 @@ export async function generateMetadata({ params }: { params: Promise<{ lang: str
   const messages: any = await getMessages({ locale: lang });
 
   return {
-    title: `Card - haveabreak`,
+    title: messages['card.title'],
     description: 'Swipe through beautiful book quotes.',
   };
 }
