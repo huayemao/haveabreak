c73439ceafc3b65fb002f03fadde3f8f6d271a34
huayemao <huayemao4g@gmail.com>
2026-05-22 17:00:21 +0800

    chore: 安卓支持签名

---
+++ apps/frame/src-tauri/gen/android/app/src/main/java/online/haveabreak/frame/MainActivity.kt
@@ -0,0 +1,11 @@
+package online.haveabreak.frame
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
